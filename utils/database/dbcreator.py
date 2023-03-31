f81b3b746fe9e8a728dd9407c21f3f2411b899e6
brousseauv <veronique.brousseauc@gmail.com>
2023-03-31 17:11:50 -0400

    add hist to MTP-cfg database

--- utils/database/dbcreator.py
+++ utils/database/dbcreator.py
@@ -2,14 +2,14 @@
 
 import argparse
 import os
-from abipy.core.structure import Structure
 from abipy.dynamics.hist import HistFile
 from ase.db import connect
-import numpy as np
 from utils.interfaces.ase_interface import abistruct_to_ase
+from utils.interfaces.mtp_interface import abistruct_to_cfg
 from utils.constants import ha_to_ev, bohr_to_ang
 
-# FIX ME: should this better be done as classes?
+# FIX ME: would this better work as classes?
+
 
 def db_from_hist(args):
 
@@ -17,22 +17,20 @@ def db_from_hist(args):
 
     structures = hist.structures
     energies = hist.etotals
-    forces = hist.reader.read_variable('fcart')
-    stresses = hist.reader.read_variable('strten')
-
+    forces = hist.reader.read_value('fcart')
+    stresses = hist.reader.read_value('strten')
+    
     db = create_database(args)
-
     dblst = list(range(0, hist.num_steps, args.mdskip))
 
     for i in dblst:
-        
+
         atoms = convert_structure(args, structures[i])
         energy = energies[i]  # in eV
         current_forces = forces[i, :, :] * ha_to_ev/bohr_to_ang  # in eV/ang
         current_stress = stresses[i, :] * ha_to_ev/(bohr_to_ang**3)  # in eV/ang^3
-        # Je suis rendue à tester ça, mais ça devrait fonctionner correctement...
         add_to_database(args, db, atoms, energy, current_forces, current_stress)
-        
+
 
 # make one function for the ASE db format and one for the MTP-MLIP .cfg format
 
@@ -44,13 +42,18 @@ def db_from_gsr(args):
     # NOTE: ase.io.read() can read the format abinit-gsr
     # I could also simply use the to_ase_atoms class method
 
+
 def create_database(args):
 
     if args.format == 'ase':
-        return  connect(args.dbname)
+        return connect(args.dbname)
 
     elif args.format == 'mtp':
-        raise NotImplementedError('MTP database creation not yet implemented')
+        if args.dbname.endswith('.cfg'):
+            return open(args.dbname, 'a')
+        else:
+            return open('{}.cfg'.format(args.dbname), 'a')
+
 
 def convert_structure(args, struct):
 
@@ -58,14 +61,17 @@ def convert_structure(args, struct):
         return abistruct_to_ase(struct)
 
     elif args.format == 'mtp':
-        raise NotImplementedError('MTP database creation not yet implemented')
+        # It should work directly from the Abipy Structure class...
+        return struct
+
 
 def add_to_database(args, db, atoms, energy, forces, stresses):
 
     if args.format == 'ase':
-        db.write(atoms, data = {'energy': energy, 'forces': forces})
+        db.write(atoms, data={'energy': energy, 'forces': forces})
     elif args.format == 'mtp':
-        raise NotImplementedError('MTP database creation not yet implemented')
+        abistruct_to_cfg(db, atoms, energy, forces, stresses)
+
 
 def create_parser():
 
@@ -75,18 +81,19 @@ def create_parser():
     data.add_argument("--path", help="Path to the calculation folder")
     data.add_argument("--fname", help="HIST.nc file name")
 
-    parser.add_argument("--source", choices=['hist', 'gsr'], help="Data source file type ('hist' for AIMD runs, 'gsr' for independent configuration)",
-            required=True)
+    parser.add_argument("--source", choices=['hist', 'gsr'], help="""Data source file type ('hist' for AIMD runs, 'gsr'
+            for independent configuration)""", required=True)
     parser.add_argument("--format", choices=['ase', 'mtp'], help="Output format for the database", required=True)
     parser.add_argument("--mdskip", type=int, default=10, help="Database will include every 'mdskip' configuration")
 
     return parser
 
+
 def check_parser(args, parser):
 
-    if args.source =='gsr' and args.path is None:
+    if args.source == 'gsr' and args.path is None:
         parser.error("--source 'gsr' requires --path argument")
-    if args.source =='hist' and args.fname is None:
+    if args.source == 'hist' and args.fname is None:
         parser.error("--source 'hist' requires --fname argument")
 
 
@@ -97,6 +104,7 @@ def main(args):
 
     elif args.source == 'gsr':
         raise NotImplementedError("Dataase creation from GSR.nc files is not yet implemented,")
+
 
 if __name__ == "__main__":
 
--- utils/interfaces/ase_interface.py
+++ utils/interfaces/ase_interface.py
@@ -2,9 +2,7 @@
 
 from ase.io import read
 
-''' Convert Abipy Structure object to ASE Atoms object '''
 
 def abistruct_to_ase(struct):
 
-    return struct.to_ase_atoms
-
+    return struct.to_ase_atoms()
