bug fix in dbsplitter, add calculation of activation energy

utils/database/dbsplitter.py
@@ -2,26 +2,55 @@ import argparse
 import numpy as np
 from ase.db import connect
 
+'''
+    These classes split a given database in ASE .db or MTP .cfg format in two distinct databases.
+    The size of the first splitted database can be specified either from the number of configurations
+    or the fraction of the total number of configurations.
+
+    Syntax:
+        python dbsplitter.py <dbname> --<OPTION> <value>
+
+    dbname: Path to the database to be splitted.
+
+    Options:
+        
+        nsplit (required, mutually exclusive with split_fraction): Integer, number of configurations in the 
+                first splitted database.
+
+        splitfraction (required, mutually exclusive with nsplit): Float between 0 and 1, fraction of the 
+                       total number of configurations for the first database, rounded to the lowest integer.
+
+        appendtxt: String containing the names that should be appended to the splitted databases.
+                   Should be a single string, with the two entries separated by a space.
+                   Default: "1 2"
+        
+    For help about these options on the command line, type 
+        python dbsplitter.py --help
+'''
 class DbSplitter:
 
-    def __init__(self, dbname, nsplit, split_fraction):
+    def __init__(self, dbname, nsplit, split_fraction, appendtxt):
 
         self.dbname = dbname
         self.nsplit = int(nsplit) if nsplit is not None else None
         self.split_fraction = float(split_fraction) if split_fraction is not None else None
+        self.appendtxt = appendtxt
 
 
 class AseDbSplitter(DbSplitter):
 
-    def __init__(self, dbname, nsplit, split_fraction):
+    def __init__(self, dbname, nsplit, split_fraction, appendtxt):
 
-        super(AseDbSplitter, self).__init__(dbname, nsplit, split_fraction)
+        super(AseDbSplitter, self).__init__(dbname, nsplit, split_fraction, appendtxt)
 
 
     def split_data(self):
 
-        out_db1, out_db2 = (self.dbname.strip('.db') + '_1.db', 
-                            self.dbname.strip('.db') + '_2.db')
+        split = self.appendtxt.split(' ')
+#        out_db1, out_db2 = (self.dbname.strip('.db') + '_1.db', 
+#                            self.dbname.strip('.db') + '_2.db')
+        out_db1, out_db2 = (self.dbname.strip('.db') + '_{}.db'.format(split[0]), 
+                            self.dbname.strip('.db') + '_{}.db'.format(split[1]))
 
         with connect(self.dbname) as db, connect(out_db1) as db1, connect(out_db2) as db2:
 
@@ -43,9 +72,9 @@ class AseDbSplitter(DbSplitter):
 
 class MtpDbSplitter(DbSplitter):
 
-    def __init__(self, dbname, nsplit, split_fraction):
+    def __init__(self, dbname, nsplit, split_fraction, appendtxt):
 
-        super(MtpDbSplitter, self).__init__(dbname, nsplit, split_fraction)
+        super(MtpDbSplitter, self).__init__(dbname, nsplit, split_fraction, appendtxt)
 
 
     def split_data(self):
@@ -61,13 +90,15 @@ class MtpDbSplitter(DbSplitter):
             current_config.append(line)
         configs.append(current_config)
 
-        out_db1, out_db2 = (self.dbname.strip('.cfg') + '_1.cfg', 
-                            self.dbname.strip('.cfg') + '_2.cfg')
+        split = self.appendtxt.split(' ')
+        out_db1, out_db2 = (self.dbname.strip('.cfg') + '_{}.cfg'.format(split[0]), 
+                            self.dbname.strip('.cfg') + '_{}.cfg'.format(split[1]))
        
         if self.split_fraction:
             ndata = int(np.floor(self.split_fraction*len(configs)))
         else:
             ndata = self.nsplit
+
         idx = np.arange(0, len(configs))
         np.random.shuffle(idx)
         idx1, idx2 = idx[:ndata], idx[ndata:]
@@ -94,18 +125,28 @@ def create_parser():
     parser.add_argument("dbname", help="path to the database file to be splitted")
     data = parser.add_mutually_exclusive_group(required=True)
     data.add_argument("--nsplit", type=int, help="number of entries in the 1st extracted database", default=None)
-    data.add_argument("--split_fraction", type=float, help="fraction of entries in the 1st extracted database, between 0 and 1",
+    data.add_argument("--fsplit", type=float, help="fraction of entries in the 1st extracted database, between 0 and 1",
                       default=None)
+    parser.add_argument("--appendtxt", default="1 2", help="Text to be appended to the splitted databases. Single string separated by a space")
     return parser
 
+def check_parser(args, parser):
+
+    if args.nsplit is None and args.fsplit is None:
+        parser.error("--nsplit or --fsplit should be passed as arguments")
+
+    if args.fsplit is not None:
+        if args.fsplit > 1.0 or args.fsplit < 0.0:
+            parser.error("--fsplit should be a float between 0 and 1")
 
 if __name__ == "__main__":
 
     parser = create_parser()
     args = parser.parse_args()
+    check_parser(args, parser)
 
     if args.dbname.endswith(".db"):
-        dbspl = AseDbSplitter(args.dbname, args.nsplit, args.split_fraction)    
+        dbspl = AseDbSplitter(args.dbname, args.nsplit, args.fsplit, args.appendtxt)    
     elif args.dbname.endswith(".cfg"):
-        dbspl = MtpDbSplitter(args.dbname, args.nsplit, args.split_fraction)    
+        dbspl = MtpDbSplitter(args.dbname, args.nsplit, args.fsplit, args.appendtxt)    
     dbspl.split_data() 

utils/diffusion/abinit_msd.py
@@ -7,10 +7,11 @@ import os
 
 class HistMsdData(MsdData):
 
-    def __init__(self, fname):
+    def __init__(self, fname, rootname='MsdData'):
 
-        super(HistMsdData, self).__init__(fname)
+        super(HistMsdData, self).__init__(fname, rootname)
         self.data = HistFile(fname)
+        self.data_source = 'Abinit HIST file'
         logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
     @property
@@ -19,32 +20,40 @@ class HistMsdData(MsdData):
         return timestep_ps
 
     @property
+    def read_temperature(self):
+        self.temperature = self.data.reader.read_value('mdtemp')[0]
+
+    @property
     def read_positions(self):
         return self.data.reader.read_value('xcart')[:, self.atom_indices, :]*bohr_to_ang
 
-    def get_atoms_for_diffusion(self, atom_type):
-        if atom_type == 'all':
+    def get_atoms_for_diffusion(self):
+        if self.atom_type == 'all':
             self.atom_indices = list(range(self.data.initial_structure.num_sites))
         else:
-            print(self.data.initial_structure.formula)
-            if str(self.data.initial_structure.formula).find(atom_type) == -1:
-                raise ValueError('Did not find atom_type {} in symbols {}'.format(atom_type, self.data.initial_structure.formula))
-            self.atom_indices = self.data.initial_structure.indices_from_symbol(atom_type)
+            if str(self.data.initial_structure.formula).find(self.atom_type) == -1:
+                raise ValueError('Did not find atom_type {} in symbols {}'.format(self.atom_type, self.data.initial_structure.formula))
+            self.atom_indices = self.data.initial_structure.indices_from_symbol(self.atom_type)
 
 
-    def compute_msd_from_positions(self, msd_type):
+    def compute_msd_from_positions(self):
 
         displacements = np.zeros((self.nframes, self.natoms, 3))
         for i in range(self.nframes):
             displacements[i, :, :] = self.traj[i, :, :] - self.traj[0, :, :]
 
-        self.msd_atoms = self.compute_atoms_msd(displacements, msd_type)
+        self.msd_atoms = self.compute_atoms_msd(displacements)
         self.msd = np.mean(self.msd_atoms, axis=1)
 
-    def compute_diffusion_coefficient(self, atom_type='all', msd_type='bare', plot=False, **kwargs):
+    def compute_diffusion_coefficient(self, atom_type='all', msd_type='bare', plot=False, plot_errors=False, **kwargs):
 
         timestep = self.read_timestep
-        self.get_atoms_for_diffusion(atom_type)
+        self.atom_type = atom_type
+        logging.info('Will average MSD(T) on {} atoms'.format(self.atom_type))
+
+        self.msd_type = msd_type
+        self.read_temperature
+        self.get_atoms_for_diffusion()
         logging.info('Extracting trajectories...')
         self.traj = self.read_positions
         self.nframes, self.natoms = np.shape(self.traj)[:2]
@@ -61,13 +70,15 @@ class HistMsdData(MsdData):
         # need : self.time, self.msd_atoms, self.msd, self.msd_std
         self.time = timestep*np.arange(self.nframes)
         logging.info('Computing MSD from atomic positions...') 
-        self.compute_msd_from_positions(msd_type)
+        self.compute_msd_from_positions()
         logging.info('... done!')
 
         self.diffusion = self.extract_diffusion_coefficient()
         self.msd_std = self.extract_msd_errors()
-        #print('Diffusion coefficient: {:.3e} cm^2/s'.format(self.diffusion))
         logging.info('Diffusion coefficient: {:.3e} cm^2/s'.format(self.diffusion))
 
+        self.write_data()
+
         if plot:
-            self.plot_diffusion_coefficient(**kwargs)
+            self.plot_errors = plot_errors
+            self.plot_diffusion_coefficient(defname='diffusion.png', **kwargs)

utils/diffusion/lammps_msd.py
@@ -7,70 +7,87 @@ import os
 
 class LammpsMsdData(MsdData):
 
-    def __init__(self, fname, filetype):
+    def __init__(self, fname, filetype, rootname='MsdData'):
 
         if filetype not in ['thermo', 'dump', 'dump-netcdf']:
             raise Exception('''filetype should be one of the following: "thermo", "dump",
                                "dump-netcdf", but I got {}'''.format(filetype))
 
         self.filetype = filetype
-        super(LammpsMsdData, self).__init__(fname)
+        super(LammpsMsdData, self).__init__(fname, rootname)
         logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
     
     def compute_diffusion_coefficient(self, thermo_fname=None, timestep=None, atom_type='all', atomic_numbers=None, 
-                                      msd_type='bare', plot=False, **kwargs):
+                                      msd_type='bare', input_temperature=None, plot=False, plot_errors=False, **kwargs):
+
+        self.msd_type = msd_type
 
         if self.filetype == 'thermo':
             if not thermo_fname:
                 thermo_fname = extract_thermo(self.fname)
+            self.data_source = 'LAMMPS thermo data'
             data = create_thermo_dataframe(thermo_fname)
             logging.info('Extracting MSD from thermo file...')
-            self.time, self.msd = read_msd_from_thermo(data)
+            self.time, self.msd, self.temperature = read_msd_from_thermo(data)
             self.msd_atoms = None
+            self.atom_type = 'See lammps input file'
+            self.nframes = len(self.msd)
+            self.natoms = None
+            self.atom_type = 'See lammps input file'
 
         elif self.filetype == 'dump':
             if not timestep:
                 raise ValueError('Missing value for timestep (in ps)')
             if not atomic_numbers:
                 raise ValueError('Must define a list for atomic_numbers')
-            if msd_type not in ['bare', 'timesliced']:
-                raise ValueError('msd_type should be either "bare" or "timesliced" but I got {}'.format(msd_type))
-            print('Will average MSD(T) on {} atoms'.format(atom_type))
+            if self.msd_type not in ['bare', 'timesliced']:
+                raise ValueError('msd_type should be either "bare" or "timesliced" but I got {}'.format(self.msd_type))
+            if not input_temperature:
+                raise ValueError('Must specify MD running temperature as input_temperature (int)')
+            elif not isinstance(input_temperature, int):
+                raise ValueError('input_temperature must be an integer, but I got a {}'.format(type(input_temperature)))
+            else:
+                self.temperature = input_temperature
+
+            self.data_source = 'LAMMPS .dump file'
+            self.atom_type = atom_type
+            logging.info('Will average MSD(T) on {} atoms'.format(self.atom_type))
 
             logging.info('Extracting trajectories...')
             self.traj = read_traj_from_dump(self.fname, atomic_numbers)
-            self.get_atoms_for_diffusion(atom_type)
+            self.get_atoms_for_diffusion()
             self.nframes = len(self.traj)
             self.natoms = len(self.atom_indices)
 
             logging.info('Computing MSD from atomic positions...')
-            self.compute_msd_from_positions(msd_type)
+            self.compute_msd_from_positions()
             logging.info('... done!')
             self.time = timestep*np.arange(len(self.msd)) # check the final shape, it should be just len=N timesteps
-
             # Just curious, does this work?!?
 #            self.coeff = self.get_diffusion_ase(timestep)
 
         elif self.filetype == 'dump-netcdf':
+            self.data_source = 'LAMMPS .dump netCDF file'
             raise NotImplementedError('"dump-netcdf" filetype not yet implemented')
 
         self.diffusion = self.extract_diffusion_coefficient()
         self.msd_std = self.extract_msd_errors()
-        print('Diffusion coefficient: {:.3e} cm^2/s'.format(self.diffusion))
         logging.info('Diffusion coefficient: {:.3e} cm^2/s'.format(self.diffusion))
 
         if plot:
-            self.plot_diffusion_coefficient(**kwargs)
+            self.plot_errors = plot_errors
+            self.plot_diffusion_coefficient(defname='diffusion.png', **kwargs)
 
+        self.write_data()
 
-    def get_atoms_for_diffusion(self, atom_type):
+    def get_atoms_for_diffusion(self):
         
-        if atom_type == 'all':
+        if self.atom_type == 'all':
             self.atom_indices = [i for i in range(len(self.traj[0].get_chemical_symbols()))]
         else:
-            if str(self.traj[0].symbols).find(atom_type) == -1:
-                raise ValueError('Did not find atom_type {} in symbols {}'.format(atom_type, self.traj[0].symbols))
+            if str(self.traj[0].symbols).find(self.atom_type) == -1:
+                raise ValueError('Did not find atom_type {} in symbols {}'.format(self.atom_type, self.traj[0].symbols))
             self.atom_indices = [i for i, symbol in enumerate(self.traj[0].get_chemical_symbols()) if "Li" in str(symbol)]
 
     def get_diffusion_ase(self, timestep):
@@ -78,14 +95,14 @@ class LammpsMsdData(MsdData):
         # Did not check if this works...
         coeff = DiffusionCoefficient(self.traj, timestep)
 
-    def compute_msd_from_positions(self, msd_type):
+    def compute_msd_from_positions(self):
 
         # Could add a cutoff for thermalization here, discarding the first N% steps and defining the t=0 at a later point in the MD run
         displacements = np.zeros((self.nframes, self.natoms, 3))
         for i, frame in enumerate(self.traj):
             displacements[i, :, :] = self.traj[i].get_positions()[self.atom_indices] - self.traj[0].get_positions()[self.atom_indices]
 
-        self.msd_atoms = self.compute_atoms_msd(displacements, msd_type)
+        self.msd_atoms = self.compute_atoms_msd(displacements)
         self.msd = np.mean(self.msd_atoms, axis=1)
 
 